ba4d74f9c7352e53c95faaff8d8faf9cbcf75b39
Nghia Nguyen <aihgn224@gmail.com>
2022-06-04 13:10:36 -0700

    changed components

--- src/App.js
+++ src/App.js
@@ -19,7 +19,20 @@ function App() {
     setCartItems([...cartItems, {...product, qty: 1}]);
   }
 }
+const onSubtract = (product) => {
+  // minus 1.
+  const exist = cartItems.find((x) => x.id === product.id);
+  if(exist.qty === 1) {
+    setCartItems(cartItems.filter((x) => x.id !== product.id));
+  } else {
+    setCartItems(
+      cartItems.map((x)=>
+      x.id === product.id ? {...exist, qty: exist.qty -1} :x)
+    );
+  }
+}
 const onRemove = (product ) => {
+  // edit to remove all
   const exist = cartItems.find((x) => x.id === product.id);
   if(exist.qty === 1) {
     setCartItems(cartItems.filter((x) => x.id !== product.id));
